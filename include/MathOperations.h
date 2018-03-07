48844a73c5dd0cde8532a69a18d4c855d3a148a0
fanfan <junminfan@126.com>
2018-03-07 17:12:33 +0800

    add some solution in PtOffer.h

--- include/MathOperations.h
+++ include/MathOperations.h
@@ -5,12 +5,62 @@
 class MathOperations
 {
 public:
-    static int addition(int, int);
-    static int substrction(int, int);
-    static int absValue(int);
-    static int oppValue(int);
-    static int multiplication(int, int);
-    static int division(int, int);
+    static int addition(int a, int b){
+        // a + b
+        /*
+        if(b == 0) return a; //没有进位的时候完成运算
+        int sum, carry;
+        sum = a ^ b;        //没有进位的加法运算
+        carry = (a & b) << 1;   //进位（左移）
+        return addition(sum, carry); //递归
+        */
+        return b == 0 ? a : addition(a ^ b, (a & b) << 1);
+    }
+
+    static int substrction(int a, int b){
+        // a - b
+        /*
+        int substractor = addition(~b, 1); //取b的补码（取反加一）
+        return addition(a, substractor);
+        */
+        return addition(a, addition(~b, 1));
+    }
+
+    static int absValue(int a){
+        // a的绝对值|a|
+        return a < 0 ? addition(~a, 1) : a;
+    }
+
+    static int oppValue(int a){
+        // a的相反数
+        return addition(~a, 1);
+    }
+
+    static int multiplication(int a, int b){
+        // a * b
+        //取绝对值
+        int multiplicand = absValue(a), multiplier = absValue(b);
+        int count = 0, product = 0;
+        //用加法计算乘法
+        while(count < multiplier){
+            product = addition(product, multiplicand);
+            count = addition(count, 1);
+        }
+        //符号判断
+        return (a ^ b) >= 0 ? product : oppValue(product);
+    }
+
+    static int division(int a, int b){
+        // a / b
+        int dividend = absValue(a), divisor = absValue(b);
+        int quotient = 0, remainder = 0;    //商和余数
+        while(divisor <= dividend){
+            quotient = addition(quotient, 1);
+            dividend = substrction(dividend, divisor);
+        }
+        //符号判断
+        return (a ^ b) >= 0 ? quotient : oppValue(quotient);
+    }
 };
 
 #endif // MATHOPERATIONS_H
--- include/PtOffer.h
+++ include/PtOffer.h
@@ -3,6 +3,7 @@
 #include "TreeNode.h"
 class PtOffer
 {
+public:
 //Tree
     //二叉树深度
     int TreeDepth(TreeNode* pRoot)
@@ -81,6 +82,99 @@ class PtOffer
         Traversal(pRoot, vec1, 1);
         return vec0 == vec1;
     }
+
+    //二维数组中的查找
+    bool Find(int target, vector<vector<int> > array) {
+        int rowLen = array.size() - 1   ;
+        int colLen = array.empty() ? 0 : array[0].size() - 1;
+        int i = rowLen, j = 0;
+        while(i >= 0 && j <= colLen){
+            if(array[i][j] == target)
+                return true;
+            else if(array[i][j] < target)
+                ++j;
+            else
+                --i;
+        }
+        return false;
+    }
+
+    //翻转单词顺序列
+    string ReverseSentence(string str) {
+        reverse(str.begin(), str.end());
+        string::iterator firstIter = str.begin(), secondIter = str.begin();
+        while(secondIter < str.end()){
+            if(*secondIter == ' '){
+                reverse(firstIter, secondIter);
+                firstIter = secondIter + 1;
+            }
+            ++secondIter;
+        }
+        reverse(firstIter, str.end());
+        return str;
+    }
+
+    //数值的整数次方
+        //递归版本
+        double Power(double base, int exponent) {
+            if(exponent == 0)
+                return 1;
+            else if(exponent < 0)
+                return Power(1 / base, -exponent);
+            else{
+                double temp = Power(base, exponent/2) * Power(base, exponent/2);
+                return temp * temp * (exponent % 2 ? base : 1);
+            }
+        }
+        //位运算版本
+        double Power2(double base, int exponent) {
+            if(exponent < 0)
+                return Power(1 / base, -exponent);
+            double re = 1;
+            while(exponent){
+                if(exponent & 1)
+                    re *= base;
+                base *= base;
+                exponent >>= 1;
+            }
+            return re;
+        }
+    //调整数组顺序使奇数位于偶数前面
+    void reOrderArray(vector<int> &array) {
+        //类似与冒泡排序，前偶后奇就交换相邻
+        int len = array.size();
+        for(int i = 0; i < len - 1; ++i){
+            for(int j = 0; j < len - i - 1; ++j){
+                if((array[j] % 2 == 0) && (array[j + 1] % 2 == 1))
+                    swap(array[j], array[j + 1]);
+            }
+        }
+    }
+
+    //斐波那契数列
+    int Fibonacci(int n) {
+        int pre = 0, last = 1;  //用来记录f(n-2)和f(n-1)
+        if(n == 0) return pre;
+        while(--n){
+            last += pre;
+            pre = last - pre;
+        }
+        return last;
+    }
+
+    //数组中出现次数超过一半的数字
+    int MoreThanHalfNum_Solution(vector<int> numbers) {
+        //使用map
+        typedef map<int, int> intIntMap;
+        typedef pair<int, int> intIntPair;
+        intIntMap m;
+        for(auto number : numbers)
+            ++m[number];
+        auto iter = max_element(m.begin(), m.end(),[](intIntPair a, intIntPair b){return a.second < b.second;});
+        //int len = numbers.size() % 2 ?  numbers.size() / 2 + 1 : numbers.size() / 2;
+        return iter->second > numbers.size() / 2 ? iter->first : 0;
+    }
+
 };
 
 #endif // PTOFFER_H
